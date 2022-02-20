8eb3071573ec4c23cf8ca9ce72f80ba1e128b489
Turan <90398677+Turan141@users.noreply.github.com>
2022-02-20 22:37:46 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fragment, useEffect, useState } from "react";
+import './styles.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        (async function fetchMyAPI() {
+            let response = await fetch(
+                "https://api.jsonbin.io/b/621265851b38ee4b33c23ba0/4"
+            );
+            response = await response.json();
+            setQuestions(response);
+        })();
+    }, []);
+
+    const data = questions.map((elem) => {
+        return (
+            <div className="questionMainWindow" key={elem.question}>
+                {elem.type === "test" ? (
+                    <TestModule props={elem} />
+                ) : (
+                    <QuestionModule props={elem} />
+                )}
+            </div>
+        );
+    });
+
+    function TestModule({ props }) {
+        let answers = props.options;
+        let answerButtons = answers.map((elem) => {
+            return <button className="answerBtn">{elem}</button>;
+        });
+
+        return (
+            <>
+                <h1>{props.question}</h1>
+                {props.image ? <div className="testDivImg"><img className="testImg" width="400px" height="150px" src={props.image} /> </div> : null}
+                <div className="answerBtnBlock">{answerButtons}</div>
+            </>
+        );
+    }
+
+    function QuestionModule({ props }) {
+        return (
+            <>
+                <h1>{props.question}</h1>
+                {props.image ? <div className="testDivImg"><img className="testImg" width="400px" height="150px" src={props.image}/> </div> : null}
+                <input type="text" />
+            </>
+        );
+    }
+
+    return (
+        <Fragment>
+            <div className="MainSection">
+                <div className="questionSection">{data}</div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
